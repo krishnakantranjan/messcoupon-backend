ec93bb9039d1f74770ff6f8cf29acce0fb21133e
KRISHNA <krishnakantranjan@krishnakants-MacBook-Air.local>
2025-07-03 20:24:40 +0530

    code again

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const connectDB = async () => {
-    await mongoose.connect(
-        'mongodb+srv://krishnakantranjan50:2NIeuWBbNfyTKIxD@krishnacluster.owtuulo.mongodb.net/MessCoupon'
-    );
-};
-
-module.exports = connectDB;
-
